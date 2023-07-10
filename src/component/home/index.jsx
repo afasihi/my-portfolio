a83ba027be39208b88afa9ab1ee665c4296007a3
Abderrahman Fasihi <a.fasihi@altados.com>
2023-07-10 16:37:08 +0100

    add home scene

--- src/component/home/index.jsx
+++ src/component/home/index.jsx
@@ -1,9 +1,43 @@
-import './home.scss'
+import { TypeAnimation } from "react-type-animation";
+import { FaLinkedinIn, FaGithub } from "react-icons/fa";
+import "./home.scss";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home">
+      <span>Abderrahman Fasihi</span>
+      <TypeAnimation
+        sequence={[
+          "Front-End Developer",
+          1500,
+          "&",
+          500,
+          "Mobile Developer",
+          1500,
+        ]}
+        wrapper="span"
+        speed={20}
+        className="type-animation"
+        repeat={Infinity}
+      />
+      <div className="social-media">
+        <a
+          href="https://www.linkedin.com/in/afasihi/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedinIn size={35} />
+        </a>
+        <a
+          href="https://github.com/afasihi"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub size={35} />
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
